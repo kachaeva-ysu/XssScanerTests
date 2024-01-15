b07b0a42a06a59fcc1da42eae99827a9171ee9b8
Nastya <Nastya@REDMI>
2024-01-15 13:36:47 +0300

    Add project files.

---
+++ XssScanerTests/Controllers/TestsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XssScanerTests.Controllers
+{
+    public class TestsController : Controller
+    {
+        public async Task<IActionResult> DOMBasedXSS()
+        {
+            // #<script>alert('XSS')</script>
+
+            return View();
+        }
+
+        public async Task<IActionResult> OutsideOfTag(string input)
+        {
+            // <script>alert('XSS')</script>
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideTagSingleQuotes(string input)
+        {
+            // foo' onerror="alert('XSS')" src name='bar
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideTagDoubleQuotes(string input)
+        {
+            // foo" onerror="alert('XSS')" src name="bar
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideTagOutsideQuotes(string input)
+        {
+            // foo onerror="alert('XSS')" src
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideComment(string input)
+        {
+            // --><script>alert('XSS')</script>
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideScriptTagSingleQuotes(string input)
+        {
+            // foo'); alert('XSS
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideScriptTagDoubleQuotes(string input)
+        {
+            // foo"); alert("XSS
+
+            ViewBag.Input = input;
+            return View();
+        }
+
+        public async Task<IActionResult> InsideScriptTagOutsideQuotes(string input)
+        {
+            // 'foo'); alert('XSS'
+
+            ViewBag.Input = input;
+            return View();
+        }
+    }
+}
---
+++ XssScanerTests/Program.cs
@@ -0,0 +1,10 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseRouting();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Tests}/{action=OutsideOfTag}");
+
+app.Run();
